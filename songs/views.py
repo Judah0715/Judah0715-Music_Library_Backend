7ba1e2da8b3dc1e7063515c167dcd2f2a482c650
Judah Agnew <judah.agnew121@gmail.com>
2023-01-30 10:34:59 -0800

    Created New View Function for Getting Songs By Id + Added New URLs Path for song_detail View Function

--- songs/views.py
+++ songs/views.py
@@ -10,10 +10,21 @@ def songs_list(request):
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
-
-
     elif request.method == 'POST':
         serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def song_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song);
+        return Response(serializer.data)
+
+
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
+
